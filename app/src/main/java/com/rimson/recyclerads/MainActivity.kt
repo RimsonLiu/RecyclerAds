4e408a239a75875e0b57a7f59ca549407a4602db
RimsonLiu <rimson@qq.com>
2018-12-25 15:34:32 +0800

    放弃ViewGroup，用FrameLayout实现初步效果

--- app/src/main/java/com/rimson/recyclerads/MainActivity.kt
+++ app/src/main/java/com/rimson/recyclerads/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,21 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val top = BitmapFactory.decodeResource(resources, R.mipmap.top)
         val bottom = BitmapFactory.decodeResource(resources, R.mipmap.bottom)
+        bottom_image.setImageBitmap(bottom)
         clear_image.setImageBitmap(top)
 
-        rav.setImageBitmap(top, bottom)
-        val topImageView = rav.topImageView
         val screenHeight = this.window.decorView.measuredHeight
-        val base = blank_view_1.measuredHeight + rav.measuredHeight - screenHeight
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            scrollView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            scrollView.setOnScrollChangeListener { _, _, scrollY, _, _ ->
                 Log.d("hehe", scrollY.toString())
-                if (scrollY > blank_view_1.measuredHeight && scrollY > oldScrollY) {
-                    topImageView.visibility = View.INVISIBLE
-                } else if (scrollY < oldScrollY) {
-                    topImageView.visibility = View.VISIBLE
-                }
+                clear_image.updateRadius(scrollY - 500)
             }
         }
     }
--- app/src/main/java/com/rimson/recyclerads/view/RecyclerImageView.kt
+++ app/src/main/java/com/rimson/recyclerads/view/RecyclerImageView.kt
@@ -7,44 +7,39 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import com.rimson.recyclerads.R
 
-class ClearImageView(context: Context, attrs: AttributeSet?, defStyle: Int) :
+class RecyclerImageView(context: Context, attrs: AttributeSet?, defStyle: Int) :
     ImageView(context, attrs, defStyle) {
-    private var mBitmap: Bitmap? = null
-    private var tempBitmap: Bitmap? = null
-    private var mStartX = 0f
-    private var mStartY = 0f
-
-    private var mPaint = Paint()
-    private var clearPaint = Paint()
-
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    init {
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+    private var mStartX = 0f
+    private var mStartY = 0f
+    private var mRadius = 0
 
-        mPaint.flags = Paint.ANTI_ALIAS_FLAG
+    private var clearPaint = Paint()
+
+    init {
+        // 关闭硬件加速
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         clearPaint.isAntiAlias = true
         clearPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.XOR)
+    }
 
-//        clearPaint.color = Color.TRANSPARENT
-//        mPaint.color = Color.RED
-//        mPaint.strokeWidth = 3f
+    fun updateRadius(r: Int) {
+        mRadius = r
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
         Log.d("hehe", "onDraw")
         super.onDraw(canvas)
         val bitmap = (drawable as BitmapDrawable).bitmap
-        bitmap.setHasAlpha(true)
-        canvas?.drawBitmap(bitmap, 0f, 0f, mPaint)
-//        val layer = canvas?.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null)
         mStartX = (measuredWidth * 0.75).toFloat()
         mStartY = (measuredHeight * 0.75).toFloat()
-        canvas?.drawCircle(mStartX, mStartY, height.toFloat() / 10, clearPaint)
-
+        canvas?.drawBitmap(bitmap, 0f, 0f, null)
+        canvas?.drawCircle(mStartX, mStartY, mRadius.toFloat(), clearPaint)
     }
+
 
 }
